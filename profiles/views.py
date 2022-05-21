953c0eb546a226f4d62a453bbab1a99622d084c9
root <root@Esonic.localdomain>
2022-05-21 23:28:39 +0300

    profiles added

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Profile
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class ProfileDetailView(DetailView):
+    template_name = "profiles/detail.html"
+    model = User
+    context_object_name = 'user'
+    http_method_names = ['get']
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
--- til/urls.py
+++ til/urls.py
@@ -17,12 +17,14 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import include
-from feed import urls as feed_urls
 from django.conf import settings
 from django.conf.urls.static import static
+from feed import urls as feed_urls
+from profiles import urls as profile_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(feed_urls, namespace="feed")),
+    path('', include(profile_urls, namespace='user')),
     re_path('', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
